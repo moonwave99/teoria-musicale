f96eb9aa81bc91a2dffe0309a4ae7b97dbd93edc
Diego Caponera <diego.caponera@gmail.com>
2022-06-04 22:40:47 +0200

    Draft support for tag highlight

---
+++ src/components/Chord.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Chord({ children }) {
+  return <span className="tag chord">{children}</span>;
+}
--- src/theme/MDXComponents/index.js
+++ src/theme/MDXComponents/index.js
@@ -2,11 +2,13 @@ import MDXComponents from "@theme-original/MDXComponents";
 import { Sequence, Part } from "@site/src/components/Sequence/Sequence";
 import Piano from "@site/src/components/Piano";
 import Table from "@site/src/components/Table";
+import Chord from "@site/src/components/Chord";
 
 export default {
   ...MDXComponents,
   Piano,
   Table,
   Sequence,
-  Part
+  Part,
+  Chord
 };
