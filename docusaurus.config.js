23377aec3205b498a316eaa0adaaab9bf5a51f54
moonwave99 <diego.caponera@gmail.com>
2024-06-03 14:28:33 +0200

    Fix truncate syntax

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,72 +6,75 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "Paino Blog",
-  tagline: "Piano, painless",
-  url: "https://your-docusaurus-test-site.com",
-  baseUrl: "/",
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
+    title: "Paino Blog",
+    tagline: "Piano, painless",
+    url: "https://your-docusaurus-test-site.com",
+    baseUrl: "/",
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
+    favicon: "img/favicon.ico",
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "mwlabs", // Usually your GitHub org/user name.
-  projectName: "paino-blog", // Usually your repo name.
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: "mwlabs", // Usually your GitHub org/user name.
+    projectName: "paino-blog", // Usually your repo name.
 
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: "en",
-    locales: ["en"]
-  },
+    // Even if you don't use internalization, you can use this field to set useful
+    // metadata like html lang. For example, if your site is Chinese, you may want
+    // to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: "en",
+        locales: ["en"],
+    },
 
-  presets: [
-    [
-      "classic",
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: false,
-        theme: {
-          customCss: [
-            require.resolve("./src/css/custom.css"),
-            require.resolve(
-              "./node_modules/@moonwave99/paino/src/styles/paino.css"
-            )
-          ]
-        }
-      })
-    ]
-  ],
+    presets: [
+        [
+            "classic",
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: false,
+                theme: {
+                    customCss: [
+                        require.resolve("./src/css/custom.css"),
+                        require.resolve(
+                            "./node_modules/@moonwave99/paino/src/styles/paino.css"
+                        ),
+                    ],
+                },
+                blog: {
+                    truncateMarker: new RegExp("<Truncate/>"),
+                },
+            }),
+        ],
+    ],
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: "Paino",
-        logo: {
-          alt: "Paino",
-          src: "img/logo.svg"
-        },
-        items: [
-          { to: "/blog", label: "Articles", position: "left" },
-          {
-            href: "https://github.com/moonwave99/paino",
-            label: "GitHub",
-            position: "right"
-          }
-        ]
-      },
-      footer: {
-        style: "dark",
-        copyright: `Copyright © ${new Date().getFullYear()} mwlabs. Built with Docusaurus.`
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme
-      }
-    })
+    themeConfig:
+        /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+        ({
+            navbar: {
+                title: "Paino",
+                logo: {
+                    alt: "Paino",
+                    src: "img/logo.svg",
+                },
+                items: [
+                    { to: "/blog", label: "Articles", position: "left" },
+                    {
+                        href: "https://github.com/moonwave99/paino",
+                        label: "GitHub",
+                        position: "right",
+                    },
+                ],
+            },
+            footer: {
+                style: "dark",
+                copyright: `Copyright © ${new Date().getFullYear()} mwlabs. Built with Docusaurus.`,
+            },
+            prism: {
+                theme: lightCodeTheme,
+                darkTheme: darkCodeTheme,
+            },
+        }),
 };
 
 module.exports = config;
--- src/theme/MDXComponents/index.js
+++ src/theme/MDXComponents/index.js
@@ -3,12 +3,14 @@ import { Sequence, Part } from "@site/src/components/Sequence/Sequence";
 import Piano from "@site/src/components/Piano";
 import Table from "@site/src/components/Table";
 import Chord from "@site/src/components/Chord";
+import Truncate from "@site/src/components/Truncate";
 
 export default {
-  ...MDXComponents,
-  Piano,
-  Table,
-  Sequence,
-  Part,
-  Chord
+    ...MDXComponents,
+    Piano,
+    Table,
+    Sequence,
+    Part,
+    Chord,
+    Truncate,
 };
