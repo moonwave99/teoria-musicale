deee636001fa2ee2bb38c34fb23870c2ba3b02b8
moonwave99 <diego.caponera@gmail.com>
2024-06-03 13:45:43 +0200

    Add sequence support.

--- src/components/Piano.js
+++ src/components/Piano.js
@@ -4,29 +4,47 @@ import cx from "clsx";
 import usePaino from "../hooks/usePaino";
 
 export default function Piano({
-  notes,
-  octaves = 3,
-  className,
-  showActions = true,
-  label
+    notes,
+    octaves = 3,
+    className,
+    showActions = true,
+    label,
+    id,
 }) {
-  const { ref, play } = usePaino({ notes, octaves });
-
-  return (
-    <div className="piano-wrapper">
-      <div className="piano-content">
-        <figure ref={ref} className={cx("paino", className)}></figure>
-        <div className="piano-sidebar">
-          {label && <span className="piano-label">{label}</span>}
+    const { ref, play } = usePaino({ notes, octaves });
+    return (
+        <div
+            className={cx("piano-wrapper", { "show-actions": showActions })}
+            id={id}
+        >
+            <div className="piano-content">
+                <figure ref={ref} className={cx("paino", className)}></figure>
+                <div className="piano-sidebar">
+                    {label && <span className="piano-label">{label}</span>}
+                </div>
+            </div>
+            {showActions && (
+                <div className="button-group">
+                    <button
+                        className="button button--sm button--primary action"
+                        onClick={play}
+                    >
+                        Play
+                    </button>
+                    <button
+                        className="button button--sm button--primary action"
+                        onClick={() => play({ speed: "rake" })}
+                    >
+                        Rake
+                    </button>
+                    <button
+                        className="button button--sm button--primary action"
+                        onClick={() => play({ speed: "arpeggio" })}
+                    >
+                        Arpeggio
+                    </button>
+                </div>
+            )}
         </div>
-      </div>
-      {showActions && (
-        <div>
-          <button onClick={play}>Play</button>
-          <button onClick={() => play({ speed: "rake" })}>Rake</button>
-          <button onClick={() => play({ speed: "arpeggio" })}>Arpeggio</button>
-        </div>
-      )}
-    </div>
-  );
+    );
 }
--- src/hooks/usePaino.js
+++ src/hooks/usePaino.js
@@ -4,37 +4,37 @@ import { parseNotes } from "../lib/utils";
 import soundEngine from "../lib/soundEngine";
 
 export default function usePaino({ notes = "", octaves = 3 }) {
-  const ref = useRef(null);
-  const pianoRef = useRef(null);
-  const parsedNotes = parseNotes(notes);
+    const ref = useRef(null);
+    const pianoRef = useRef(null);
+    const parsedNotes = parseNotes(notes);
 
-  useEffect(() => {
-    if (pianoRef.current) {
-      return;
+    useEffect(() => {
+        if (pianoRef.current) {
+            return;
+        }
+        pianoRef.current = new Paino({
+            el: ref.current,
+            octaves,
+            startOctave: 2,
+        });
+        pianoRef.current.render();
+    }, [octaves]);
+
+    useEffect(() => {
+        if (parsedNotes) {
+            pianoRef.current.setNotes(parsedNotes);
+        }
+        return () => {
+            pianoRef.current.clearNotes();
+        };
+    }, [parsedNotes]);
+
+    async function play({ speed }) {
+        await soundEngine.play({
+            notes: parsedNotes.notes,
+            speed,
+        });
     }
-    pianoRef.current = new Paino({
-      el: ref.current,
-      octaves,
-      startOctave: 2
-    });
-    pianoRef.current.render();
-  }, [octaves]);
 
-  useEffect(() => {
-    if (parsedNotes) {
-      pianoRef.current.setNotes(parsedNotes);
-    }
-    return () => {
-      pianoRef.current.clearNotes();
-    };
-  }, [parsedNotes]);
-
-  async function play({ speed }) {
-    await soundEngine.play({
-      notes: parsedNotes.notes,
-      speed
-    });
-  }
-
-  return { ref, parsedNotes, play };
+    return { ref, parsedNotes, play };
 }
