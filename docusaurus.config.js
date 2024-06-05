a1e278c2854eb23a613ca0dcebf29fcf99b746f3
moonwave99 <diego.caponera@gmail.com>
2024-06-05 22:54:29 +0200

    Update footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -79,7 +79,7 @@ const config = {
             },
             footer: {
                 style: "dark",
-                copyright: `Copyright © ${new Date().getFullYear()} mwlabs. Built with Docusaurus.`,
+                copyright: `Copyright © ${new Date().getFullYear()} Diego Caponera.`,
             },
             prism: {
                 theme: lightCodeTheme,
